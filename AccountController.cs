efe6cb09af445e8ff7383b93a086555a7baa50f2
dianapopova21 <112687113+dianapopova21@users.noreply.github.com>
2022-11-19 21:35:32 +0200

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tests.Models;
+using Tests.ViewModels;
+
+namespace Tests.Controllers
+{
+    public class AccountController : Controller
+    {
+
+            private readonly UserManager<User> _userManager;
+            private readonly SignInManager<User> _signInManager;
+            private readonly RoleManager<IdentityRole> _roleManager;
+
+            public AccountController(UserManager<User> userManager, SignInManager<User> signInManager, RoleManager<IdentityRole> roleManager)
+            {
+                _userManager = userManager;
+                _signInManager = signInManager;
+                _roleManager = roleManager;
+            }
+            public IActionResult Index() => View();
+
+            [HttpGet]
+            public IActionResult Register() => View();
+
+            [HttpPost]
+            public async Task<IActionResult> Register(RegisterViewModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    User user = new User() { UserName = model.Email, Email = model.Email, Name = model.Name };
+                    var result = await _userManager.CreateAsync(user, model.Password);
+                    if (result.Succeeded)
+                    {
+                    //******
+                        await _signInManager.SignInAsync(user, false);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        foreach (var elem in result.Errors)
+                        {
+                            ModelState.AddModelError("", elem.Description);
+                        }
+                    }
+                }
+                return View(model);
+            }
+
+            [HttpGet]
+            public IActionResult Login(string returnUrl = null) => View(new LoginViewModel { ReturnUrl = returnUrl });
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public async Task<IActionResult> Login(LoginViewModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    var result =
+                        await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                    if (result.Succeeded)
+                    {
+                        if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                        {
+                            return Redirect(model.ReturnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                    }
+                }
+                return View(model);
+            }
+
+            public async Task<IActionResult> Logout()
+            {
+                await _signInManager.SignOutAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+}
---
+++ RegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tests.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Имя")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтвердите пароль")]
+        public string PasswordConfirm { get; set; }
+    }
+}
